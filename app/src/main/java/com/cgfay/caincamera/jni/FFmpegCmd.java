5c111eb9198d4bdee7e514b7d2c99e3bfa100ba5
cain <cain.huang@outlook.com>
2017-12-28 12:01:06 +0800

    add some ffmpeg command

--- app/src/main/java/com/cgfay/caincamera/jni/FFmpegCmd.java
+++ app/src/main/java/com/cgfay/caincamera/jni/FFmpegCmd.java
@@ -82,6 +82,228 @@ public class FFmpegCmd {
     }
 
     /**
+     * 裁剪视频大小 (x, y - (x + cropWidth), (y + cropHeight))
+     * @param srcVideo 原路径
+     * @param desVideo 输出路径
+     * @param bitRate  视频码率（2M = 2048, 1.5M = 1536）
+     * @param cropWidth 裁剪的宽度
+     * @param cropHeight 裁剪的高度
+     * @param x         裁剪的起始位置x
+     * @param y         裁剪的起始位置y
+     * @return
+     */
+    public static boolean cutVideoSize(String srcVideo, String desVideo, int bitRate,
+                                       int cropWidth, int cropHeight, int x, int y) {
+        // 裁剪视频的命令如下：
+        // ./ffmpeg -i 2x.mp4 -filter_complex "[0:v]setpts=0.5*PTS[v];[0:a]atempo=2.0[a]" -map "[v]" -map "[a]" output3.mp4
+        String filter = String.format(Locale.getDefault(), "crop=%d:%d:%d:%d", cropWidth, cropHeight, x, y);
+        ArrayList<String> cmds = new ArrayList<>();
+        cmds.add("ffmpeg");
+        cmds.add("-i");
+        cmds.add(srcVideo);
+        cmds.add("-vf");
+        cmds.add("" + filter);
+        cmds.add("-acodec");
+        cmds.add("copy");
+        cmds.add("-b:v");
+        int rate = (int)(bitRate * 1.5f);
+        cmds.add(rate + "k");
+        cmds.add("-y");
+        cmds.add(desVideo);
+
+        String[] commands = cmds.toArray(new String[cmds.size()]);
+
+        int result = runSafely(commands);
+
+        return (result == 1);
+    }
+
+    /**
+     * 剪切视频时长(startTime, endTime)
+     * @param srcVideo
+     * @param desVideo
+     * @param startTime
+     * @param endTime
+     * @return
+     */
+    public static boolean cutVideoTime(String srcVideo, String desVideo,
+                                       int startTime, int endTime) {
+
+        String startStr = "00:00:00";
+        String endStr = "00:00:00";
+        if (startTime < 0 || endTime < 0 || startTime >= endTime) {
+            return false;
+        }
+        // 计算开始时间
+        if (startTime < 10) {// 小于10秒
+            startStr = "00:00:0" + startTime;
+        } else if (startTime < 60) {//小于60秒
+            startStr = "00:00:" + startTime;
+        } else if (startTime < 300) { // 大于60秒时，小于5分钟
+            int endwidth = startTime % 60;
+            if (endwidth < 10) { // 尾数小于10
+                startStr = "00:0" + (startTime / 60) + ":0" + endwidth;
+            } else {
+                startStr = "00:0" + (startTime / 60) + ":" + endwidth;
+            }
+        }
+
+        // 计算结束时间
+        if (endTime < 10) { // 小于10秒
+            endStr = "00:00:0" + endTime;
+        } else if (endTime < 60) { // 小于60秒
+            endStr = "00:00:" + endTime;
+        } else if (endTime < 300) { // 小于5分钟
+            int endWidth = endTime % 60;
+            if (endWidth < 10) {
+                endStr = "00:0" + (endTime / 60) + ":0" + endWidth;
+            } else {
+                endStr = "00:0" + (endTime / 60) + ":" + endWidth;
+            }
+        }
+        ArrayList<String> cmds = new ArrayList<>();
+        cmds.add("ffmpeg");
+        cmds.add("-i");
+        cmds.add(srcVideo);
+        cmds.add("-vcodec");
+        cmds.add("copy");
+        cmds.add("-acodec");
+        cmds.add("copy");
+        cmds.add("-ss");
+        cmds.add(startStr);
+        cmds.add("-t");
+        cmds.add(endStr);
+        cmds.add(desVideo);
+
+        String[] commands = cmds.toArray(new String[cmds.size()]);
+
+        int result = runSafely(commands);
+
+        return (result == 1);
+    }
+
+
+    /**
+     * 调整播放速度
+     * @param srcVideo
+     * @param speed
+     * @param desVideo
+     * @param callback
+     */
+    public static void adjustVideoSpeed(String srcVideo, float speed,
+                                        String desVideo, OnCompletionListener callback) {
+        // ffmpeg命令
+        //./ffmpeg -i 2x.mp4 -filter_complex "[0:v]setpts=0.5*PTS[v];[0:a]atempo=2.0[a]" -map "[v]" -map "[a]" output3.mp4
+        String filter = String.format(Locale.getDefault(), "[0:v]setpts=%f*PTS[v];[0:a]atempo=%f[a]", 1/speed, speed);
+
+        ArrayList<String> cmds = new ArrayList<>();
+        cmds.add("ffmpeg");
+        cmds.add("-i");
+        cmds.add(srcVideo);
+        cmds.add("-filter_complex");
+        cmds.add(filter);
+        cmds.add("-map");
+        cmds.add("[v]");
+        cmds.add("-map");
+        cmds.add("[a]");
+        cmds.add("-y");
+        cmds.add(desVideo);
+
+        runSync(cmds, callback);
+    }
+
+    /**
+     * 合成图片到视频(图片的尺寸需要提前调整)
+     * @param imagePath 需要合成的图片路径
+     * @param srcVideo 需要合成的视频路径
+     * @param desVideo 合成的视频路径
+     * @return
+     */
+    public static boolean mergeImageToVideo(String imagePath,
+                                            String srcVideo, String desVideo) {
+        // ffmpeg命令：
+        //ffmpeg -i videoPath -i imagePath -filter_complex overlay=0:0 -vcodec libx264 -profile:v baseline -preset ultrafast -b:v 3000k -g 30 -f mp4 outPath
+        ArrayList<String> cmds = new ArrayList<>();
+        cmds.add("ffmpeg");
+        cmds.add("-i");
+        cmds.add(srcVideo);
+        cmds.add("-i");
+        cmds.add("copy");
+        cmds.add("-i");
+        cmds.add(imagePath);
+        cmds.add("-filter_complex");
+        cmds.add("overlay=0:0");
+        cmds.add("-vcodec libx264 -profile:v baseline -preset ultrafast -b:v 3000k -g 25");
+        cmds.add("-f mp4");
+        cmds.add(desVideo);
+
+        String[] commands = cmds.toArray(new String[cmds.size()]);
+        int result = runSafely(commands);
+
+        return (result == 1);
+    }
+
+
+    /**
+     * MP4转TS
+     * @param srcVideo MP4视频路径
+     * @param desVideo TS输出路径
+     * @return
+     */
+    public static boolean mp4ToTs(String srcVideo, String desVideo) {
+        // FFmpeg命令
+        //./ffmpeg -i 0.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts ts0.ts
+        ArrayList<String> cmds = new ArrayList<>();
+        cmds.add("ffmpeg");
+        cmds.add("-i");
+        cmds.add(srcVideo);
+        cmds.add("-c");
+        cmds.add("copy");
+        cmds.add("-bsf:v");
+        cmds.add("h264_mp4toannexb");
+        cmds.add("-f");
+        cmds.add("mpegts");
+        cmds.add(desVideo);
+
+        String[] commands = cmds.toArray(new String[cmds.size()]);
+        int result = runSafely(commands);
+
+        return (result == 1);
+    }
+
+    /**
+     * TS合成MP4
+     * @param tsPaths  TS流列表
+     * @param desVideo 输出MP4视频路径
+     * @return
+     */
+    public static boolean tsToMp4(List<String> tsPaths, String desVideo) {
+        // FFmpeg命令
+        //ffmpeg -i "concat:ts0.ts|ts1.ts|ts2.ts|ts3.ts" -c copy -bsf:a aac_adtstoasc out2.mp4
+        ArrayList<String> cmds = new ArrayList<>();
+        cmds.add("ffmpeg");
+        cmds.add("-i");
+        String concat="concat:";
+        for (String part : tsPaths) {
+            concat += part;
+            concat += "|";
+        }
+        concat = concat.substring(0, concat.length()-1);
+        cmds.add(concat);
+        cmds.add("-c");
+        cmds.add("copy");
+        cmds.add("-bsf:a");
+        cmds.add("aac_adtstoasc");
+        cmds.add("-y");
+        cmds.add(desVideo);
+        String[] commands = cmds.toArray(new String[cmds.size()]);
+        int result = runSafely(commands);
+
+        return (result == 1);
+    }
+
+
+    /**
      * 音视频混合
      * @param srcVideo      视频路径
      * @param videoVolume   视频声音
@@ -158,65 +380,6 @@ public class FFmpegCmd {
         runSync(cmds, callback);
 
         return true;
-    }
-
-    /**
-     * 设置播放速度
-     * @param srcVideo
-     * @param speed
-     * @param desVideo
-     * @param callback
-     */
-    public static void setPlaybackSpeed(String srcVideo, float speed,
-                                        String desVideo, OnCompletionListener callback) {
-        ArrayList<String> cmds = new ArrayList<>();
-        cmds.add("ffmpeg");
-        cmds.add("-i");
-        cmds.add(srcVideo);
-
-        cmds.add("-y");
-        cmds.add("-filter_complex");
-        cmds.add("[0:v]setpts=" + speed + "*PTS[v];[0:a]atempo=" + 1 / speed + "[a]");
-        cmds.add("-map");
-        cmds.add("[v]");
-        cmds.add("-map");
-        cmds.add("[a]");
-        cmds.add(desVideo);
-
-        runSync(cmds, callback);
-    }
-
-
-    /**
-     * 剪切视频
-     * @param srcVideo
-     * @param desVideo
-     * @param startTime
-     * @param endTime
-     * @return
-     */
-    public static boolean cutVideo(String srcVideo, String desVideo,
-                                   float startTime, float endTime) {
-
-        ArrayList<String> cmds = new ArrayList<>();
-        cmds.add("ffmpeg");
-        cmds.add("-i");
-        cmds.add(srcVideo);
-
-        cmds.add("-y");
-        cmds.add("-ss");
-        cmds.add("" + startTime);
-        cmds.add("-t");
-        cmds.add("" + endTime);
-        cmds.add("-c");
-        cmds.add("copy");
-        cmds.add(desVideo);
-
-        String[] commands = cmds.toArray(new String[cmds.size()]);
-
-        int result = runSafely(commands);
-
-        return (result == 1);
     }
 
     /**
@@ -357,7 +520,7 @@ public class FFmpegCmd {
     }
 
     /**
-     * 合并多个视频
+     * 合并多个视频（Bug：不同滤镜的视频合成失败）
      * @param videoPathList 视频列表
      * @param desVideo      输出视频
      * @return
@@ -429,30 +592,6 @@ public class FFmpegCmd {
         cmds.add("-f");
         cmds.add("mjpeg");
         cmds.add(picPath);
-
-        runSync(cmds, callback);
-    }
-
-    /**
-     * 裁剪视频
-     * @param srcPath   视频路径
-     * @param x         x起始坐标
-     * @param y         y起始坐标
-     * @param width     宽度
-     * @param height    高度
-     * @param destPath  目标路径
-     * @param callback  回调
-     */
-    public static void cropVideo(String srcPath, int x, int y, int width, int height,
-                                 String destPath, OnCompletionListener callback) {
-        ArrayList<String> cmds = new ArrayList<>();
-        cmds.add("ffmpeg");
-        cmds.add("-y");
-        cmds.add("-i");
-        cmds.add(srcPath);
-        cmds.add("-filter:v");
-        cmds.add("crop=" + width + ":" + height + ":" + x + ":" + y);
-        cmds.add(destPath);
 
         runSync(cmds, callback);
     }
